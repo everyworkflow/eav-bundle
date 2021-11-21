e61b4d520f41c1119128bb3e3843e4177b887ef9
Suson Waiba <susonwaiba@gmail.com>
2021-11-21 05:06:46 +0000

    Added ?ExtensionInterface

--- EveryWorkflowEavBundle.php
+++ EveryWorkflowEavBundle.php
@@ -9,11 +9,12 @@ declare(strict_types=1);
 namespace EveryWorkflow\EavBundle;
 
 use EveryWorkflow\EavBundle\DependencyInjection\EavExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class EveryWorkflowEavBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         return new EavExtension();
     }
