ad51f41b5811c66f708c905d76fe37fd755640b4
Suson Waiba <susonwaiba@gmail.com>
2022-01-18 17:49:38 +0000

    January update fixes

--- Repository/AttributeRepository.php
+++ Repository/AttributeRepository.php
@@ -9,8 +9,11 @@ declare(strict_types=1);
 namespace EveryWorkflow\EavBundle\Repository;
 
 use EveryWorkflow\EavBundle\Attribute\BaseAttribute;
+use EveryWorkflow\EavBundle\Attribute\BaseAttributeInterface;
+use EveryWorkflow\EavBundle\Factory\AttributeFactoryInterface;
 use EveryWorkflow\MongoBundle\Repository\BaseDocumentRepository;
 use EveryWorkflow\MongoBundle\Support\Attribute\RepositoryAttribute;
+use Symfony\Contracts\Service\Attribute\Required;
 
 #[RepositoryAttribute(
     documentClass: BaseAttribute::class,
@@ -19,5 +22,26 @@ use EveryWorkflow\MongoBundle\Support\Attribute\RepositoryAttribute;
 )]
 class AttributeRepository extends BaseDocumentRepository implements AttributeRepositoryInterface
 {
-    // Something
+    protected ?AttributeFactoryInterface $attributeFactory = null;
+
+    #[Required]
+    public function setAttributeFactory(AttributeFactoryInterface $attributeFactory): self
+    {
+        $this->attributeFactory = $attributeFactory;
+        return $this;
+    }
+
+    public function getAttributeFactory(): ?AttributeFactoryInterface
+    {
+        return $this->attributeFactory;
+    }
+
+    public function create(array $data = []): BaseAttributeInterface
+    {
+        if ($this->getAttributeFactory()) {
+            return $this->getAttributeFactory()->createAttribute($data);
+        }
+        
+        return parent::create($data);
+    }
 }
--- Repository/AttributeRepositoryInterface.php
+++ Repository/AttributeRepositoryInterface.php
@@ -9,13 +9,14 @@ declare(strict_types=1);
 namespace EveryWorkflow\EavBundle\Repository;
 
 use EveryWorkflow\EavBundle\Attribute\BaseAttributeInterface;
-use EveryWorkflow\MongoBundle\Document\BaseDocumentInterface;
+use EveryWorkflow\EavBundle\Factory\AttributeFactoryInterface;
 use EveryWorkflow\MongoBundle\Repository\BaseDocumentRepositoryInterface;
 
 interface AttributeRepositoryInterface extends BaseDocumentRepositoryInterface
 {
-    /**
-     * @return BaseAttributeInterface
-     */
-    public function create(array $data = []): BaseDocumentInterface;
+    public function setAttributeFactory(AttributeFactoryInterface $attributeFactory): self;
+
+    public function getAttributeFactory(): ?AttributeFactoryInterface;
+
+    public function create(array $data = []): BaseAttributeInterface;
 }
